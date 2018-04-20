a6880653c82839cbc633465e64972da607a0e67f
MahipalreddyG <37070078+MahipalreddyG@users.noreply.github.com>
2018-04-20 16:39:23 +0530

    Update Cassandra.tf

--- Cassandra.tf
+++ Cassandra.tf
@@ -4,6 +4,8 @@ provider "aws" {
   region     = "us-west-2"
 }
 
+# Launching 3 Instances
+
 resource "aws_instance" "hello" {
   ami = "ami-4e79ed36"
   instance_type = "t2.micro"
@@ -18,13 +20,22 @@ resource "aws_instance" "hello" {
   type = "ssh"
   private_key="${file("/home/ubuntu/aws.pem")}"
   }
+
+# Capture the IPs of recently launched
+  
   provisioner "local-exec" {
      command = "echo ${self.private_ip} >> /tmp/ips.txt"
   }
+  
+# Copying the IPs file into remote machine
+  
   provisioner "file" {
     source      = "/tmp/ips.txt"
     destination = "/tmp/ips.txt"
   }
+  
+# Using script.sh install the Cassandra on 3 instances
+  
   provisioner "file" {
     source      = "/home/ubuntu/terraform/script.sh"
     destination = "/tmp/script.sh"
@@ -35,16 +46,25 @@ resource "aws_instance" "hello" {
       "/tmp/script.sh args",
       ]
   }
+  
+# Give the full permissions to /etc/cassandra/
+  
   provisioner "remote-exec" {
     inline = [
         "sudo chmod 777 /etc/cassandra/",
         "sudo mv /etc/cassandra/cassandra.yaml /etc/cassandra/cassandra.yaml.bkp",
       ]
   }
+  
+# Replace the cassandra.yaml file with our own cassandra.yaml for easylly change the parameters
+  
   provisioner "file" {
     source      = "/home/ubuntu/cassandra.yaml"
     destination = "/etc/cassandra/cassandra.yaml"
   }
+
+# Do the changes in cassandra.yaml using with config.sh
+  
   provisioner "file" {
     source      = "/home/ubuntu/config.sh"
     destination = "/tmp/config.sh"
